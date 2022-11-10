ffb8137fd4cf8ba3fbe035f4a45020ebc65b1b2a
akshaykanherkar7 <akshaykanherkar7@gmail.com>
2022-11-10 10:57:15 +0530

    Added: Timer

--- src/Components/Timer/Timer.jsx
+++ src/Components/Timer/Timer.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Timer = () => {
-  return (
-    <div>Timer</div>
-  )
-}
+  const [timer, setTimer] = useState(0);
+  const [value, setValue] = useState(0);
+  const [toggle, setToggle] = useState(true);
 
-export default Timer
+  useEffect(() => {
+    let id = setInterval(() => {
+      if (timer < 1) {
+        clearInterval(id);
+      } else {
+        setTimer((prev) => prev - 1);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(id);
+    };
+  }, [timer]);
+
+  const handleOnClick = () => {
+    setTimer(value);
+    if (toggle) {
+      setToggle(false);
+    } else {
+      setToggle(true);
+      setTimer(0);
+    }
+  };
+  return (
+    <div>
+      <h1>Timer</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <br />
+      <div>{timer}</div>
+      <button onClick={handleOnClick}>{toggle ? "Start" : "Reset"}</button>
+    </div>
+  );
+};
+
+export default Timer;
